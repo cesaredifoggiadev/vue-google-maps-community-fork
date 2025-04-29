eaed5f233ce33e1c024dd1b63878f3444659205a
Cesare di Foggia <cesaredifoggiadev@gmail.com>
2025-04-29 09:42:08 +0000

    fix(#1): add library marker

--- src/components/marker.vue
+++ src/components/marker.vue
@@ -1,133 +1,92 @@
 <template>
-  <div
-    @click="
-      () => {
-        console.log('sdfsd')
-      }
-    "
-  >
-    <slot></slot>
-  </div>
+  <div></div>
 </template>
+
 <script>
-import buildComponent from './build-component.js'
-
-const props = {
-  animation: {
-    twoWay: true,
-    type: Number,
-  },
-  attribution: {
-    type: Object,
-  },
-  clickable: {
-    type: Boolean,
-    twoWay: true,
-    default: true,
-  },
-  cursor: {
-    type: String,
-    twoWay: true,
-  },
-  draggable: {
-    type: Boolean,
-    twoWay: true,
-    default: false,
-  },
-  icon: {
-    twoWay: true,
-  },
-  label: {},
-  opacity: {
-    type: Number,
-    default: 1,
-  },
-  options: {
-    type: Object,
-  },
-  place: {
-    type: Object,
-  },
-  position: {
-    type: Object,
-    twoWay: true,
-  },
-  shape: {
-    type: Object,
-    twoWay: true,
-  },
-  title: {
-    type: String,
-    twoWay: true,
-  },
-  zIndex: {
-    type: Number,
-    twoWay: true,
-  },
-  visible: {
-    twoWay: true,
-    default: true,
-  },
-}
-
-const events = [
-  'click',
-  'rightclick',
-  'dblclick',
-  'drag',
-  'dragstart',
-  'dragend',
-  'mouseup',
-  'mousedown',
-  'mouseover',
-  'mouseout',
-]
-
-export default buildComponent({
-  mappedProps: props,
-  events,
-  name: 'marker',
-  ctr: () => google.maps.marker.AdvancedMarkerElement,
-
-  inject: {
-    $clusterPromise: {
+export default {
+  name: 'GMapMarker',
+  props: {
+    position: {
+      type: Object,
+      required: true,
+    },
+    map: {
+      type: Object,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    draggable: {
+      type: Boolean,
+      default: false,
+    },
+    zIndex: {
+      type: Number,
       default: null,
     },
+    content: {
+      type: [String, Object],
+      default: null, // per custom HTML nel marker
+    },
   },
-  emits: events,
+  data() {
+    return {
+      markerObject: null,
+    };
+  },
+  watch: {
+    position: {
+      handler(val) {
+        if (this.markerObject) {
+          this.markerObject.position = val;
+        }
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    this.addMarker();
+  },
   unmounted() {
-    if (!this.$markerObject) {
-      return
-    }
-
-    if (this.$clusterObject) {
-      // Repaint will be performed in `updated()` of cluster
-      this.$clusterObject.removeMarker(this.$markerObject, true)
-    } else {
-      this.$markerObject.setMap(null)
-    }
+    this.removeMarker();
   },
+  methods: {
+    addMarker() {
+      if (!google.maps.marker?.AdvancedMarkerElement) {
+        console.error('AdvancedMarkerElement non disponibile. Hai caricato la libreria "marker"?');
+        return;
+      }
 
-  beforeCreate(options) {
-    if (this.$clusterPromise) {
-      options.map = null
-    }
+      const options = {
+        map: this.map,
+        position: this.position,
+        title: this.title,
+        zIndex: this.zIndex,
+        draggable: this.draggable,
+      };
 
-    return this.$clusterPromise
+      if (this.content) {
+        if (typeof this.content === 'string') {
+          const contentDiv = document.createElement('div');
+          contentDiv.innerHTML = this.content;
+          options.content = contentDiv;
+        } else if (this.content instanceof HTMLElement) {
+          options.content = this.content;
+        }
+      }
+
+      this.markerObject = new google.maps.marker.AdvancedMarkerElement(options);
+
+      this.$emit('marker-created', this.markerObject);
+    },
+    removeMarker() {
+      if (this.markerObject) {
+        this.markerObject.map = null;
+        this.markerObject = null;
+      }
+    },
   },
-
-  afterCreate(inst) {
-    events.forEach((event) => {
-      inst.addListener(event, (payload) => {
-        this.$emit(event, payload)
-      })
-    })
-    if (this.$clusterPromise) {
-      this.$clusterPromise.then((co) => {
-        this.$clusterObject = co
-        co.addMarker(inst)
-      })
-    }
-  },
-})
+};
 </script>
